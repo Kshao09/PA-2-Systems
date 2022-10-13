96e0729924087c53a1b91afc1899706ba1825054
Kenny Shao <83423941+Kshao09@users.noreply.github.com>
2022-10-13 18:04:07 -0400

    Update print.c

--- print.c
+++ print.c
@@ -1,31 +1,14 @@
 #include "find.h"
-void print_results(char* pattern, int matched, int first_occurrence, int numbered, int sorted, int partial, int reversed, int no_of_results){
-	char** sentences = (char**) malloc(no_of_results*sizeof(char*));
-	for(int i = 0; i < no_of_results;i++){
-		sentences[i] = strdup(lineptr[results[i]]);
-	}
-	if(sorted){
-		//call qsort on sentences...
-		
-		
-	}else if (reversed){
-		//reverse the sentenes....
-	}
-	for(int i = 0; i < no_of_results;i++){
-		if(numbered)
-			printf("%d. ", results[i] + 1); 
-		if(first_occurrence)
-			if(matched)
-				printf("@%d: ", strstr_fully_matched(sentences[i], pattern) - sentences[i]);
-			else
-				printf("@%d: ", strstr(sentences[i], pattern) - sentences[i]);
-		printf("%s\n", sentences[i]);
-	}
+void swap(char *v[], int i, int j) {
+    char *temp;
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
 }
 
-void q_sort(char *v[], int left, int right) {
+void quickSort(char *v[], int left, int right) {
     int i, last;
-    void swap(char *v, int i, int j);
+    swap(char *v, int i, int j);
     
     if (left >= right) {
         return;
@@ -39,13 +22,29 @@ void q_sort(char *v[], int left, int right) {
     }
     
     swap(v, left, last);
-    q_sort(v, left, last-1);
-    q_sort(v, last+1, right);
+    quickSort(v, left, last-1);
+    quickSort(v, last+1, right);
 }
 
-void swap(char *v[], int i, int j) {
-    char *temp;
-    temp = v[i];
-    v[i] = v[j];
-    v[j] = temp;
+void print_results(char* pattern, int matched, int first_occurrence, int numbered, int sorted, int partial, int reversed, int no_of_results){
+	char** sentences = (char**) malloc(no_of_results*sizeof(char*));
+	for(int i = 0; i < no_of_results;i++){
+		sentences[i] = strdup(lineptr[results[i]]);
+	}
+	if(sorted){
+		//call qsort on sentences...
+		quickSort(sentences, 0, strlen(sentences));
+	}else if (reversed){
+		//reverse the sentenes....
+	}
+	for(int i = 0; i < no_of_results;i++){
+		if(numbered)
+			printf("%d. ", results[i] + 1); 
+		if(first_occurrence)
+			if(matched)
+				printf("@%d: ", strstr_fully_matched(sentences[i], pattern) - sentences[i]);
+			else
+				printf("@%d: ", strstr(sentences[i], pattern) - sentences[i]);
+		printf("%s\n", sentences[i]);
+	}
 }

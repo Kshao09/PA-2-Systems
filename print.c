83abe7f0eecb56ffdb5efc66ed6163a352983fd6
Kenny Shao <83423941+Kshao09@users.noreply.github.com>
2022-10-13 11:27:57 -0400

    Create print.c

---
+++ print.c
@@ -0,0 +1,51 @@
+#include "find.h"
+void print_results(char* pattern, int matched, int first_occurrence, int numbered, int sorted, int partial, int reversed, int no_of_results){
+	char** sentences = (char**) malloc(no_of_results*sizeof(char*));
+	for(int i = 0; i < no_of_results;i++){
+		sentences[i] = strdup(lineptr[results[i]]);
+	}
+	if(sorted){
+		//call qsort on sentences...
+		
+		
+	}else if (reversed){
+		//reverse the sentenes....
+	}
+	for(int i = 0; i < no_of_results;i++){
+		if(numbered)
+			printf("%d. ", results[i] + 1); 
+		if(first_occurrence)
+			if(matched)
+				printf("@%d: ", strstr_fully_matched(sentences[i], pattern) - sentences[i]);
+			else
+				printf("@%d: ", strstr(sentences[i], pattern) - sentences[i]);
+		printf("%s\n", sentences[i]);
+	}
+}
+
+void q_sort(char *v[], int left, int right) {
+    int i, last;
+    void swap(char *v, int i, int j);
+    
+    if (left >= right) {
+        return;
+    }
+    swap(v, left, (left + right) / 2);
+    last = left;
+    for(i = left + 1; i <= right; i++) {
+        if (strcmp(v[i], v[left]) < 0) {
+            swap(v, ++last, i);
+        }
+    }
+    
+    swap(v, left, last);
+    q_sort(v, left, last-1);
+    q_sort(v, last+1, right);
+}
+
+void swap(char *v[], int i, int j) {
+    char *temp;
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}

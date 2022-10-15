05caf6abde00fa8dd05b611e8a32774e704c43f0
Kenny Shao <83423941+Kshao09@users.noreply.github.com>
2022-10-15 07:24:20 -0400

    Update find.h

--- find.h
+++ find.h
@@ -1,7 +1,9 @@
-#include<stdlib.h>
-#include<string.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 char* strstr_fully_matched(char* haystack, char* needle);
-void print_results(char* pattern, int matched, int first_occurrence, int numbered, int sorted, int partial, int reversed, int no_of_results);
-extern char*lineptr[];
+void print_results(char* pattern, int matched, int first_occurrence, int numbered, int sorted, int partial, int reversed, int ignore_case, int no_of_results);
+
+extern char* lineptr[];
 extern int results[];

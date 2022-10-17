caf32cf6dca5b478c92418dcd6a60170b807717d
Kenny Shao <83423941+Kshao09@users.noreply.github.com>
2022-10-17 09:18:12 -0400

    Update find.c

--- find.c
+++ find.c
@@ -10,12 +10,6 @@
 char* lineptr[MAX_LINES];
 int results[MAX_LINES];
 
-char* str_tolower(char* input){
-    char* rv = strdup(input);
-    for(int i = 0; i < strlen(rv);i++)
-        rv[i] = tolower(rv[i]);
-    return rv;
-}
 char *strstr_fully_matched(char *haystack, char *needle) {
   char *rv;
   char padded_needle[strlen(needle) + 3];
@@ -70,7 +64,7 @@ int readlines(char** lineptr, int maxlines) {
 }
 
 int main(int argc, char** argv) {
-    int except = 0;//0 or 1
+  int except = 0;//0 or 1
 	int sorted = 0;//0 or 1
 	int reversed = 0;//0 or 1
 	int partial = 0;//0 or 1
@@ -78,57 +72,49 @@ int main(int argc, char** argv) {
 	int first_occurrence = 0;//0 or 1
 	int matched = 0;//0 or 1
 	int ignore_case = 0;//0 or 1
-	char* pattern = "";
-
 	int current_char;
 	
-	if (argc == 3) {
-		while (--argc > 1 && (*++argv)[0] == '-') {
-			while (current_char = *++argv[0])
-				switch (current_char) {
-					case 's':
-						sorted = 1;
-						break;
-					case 'r':
-						reversed = 1;
-						break;
-					case 'x':
-						except = 1;
-						break;
-					case 'm':
-						matched = 1;
-						break;
-					case 'n':
-						numbered = 1;
-						break;
-					case 'f':
-						first_occurrence = 1;
-						break;
-					case 'p':
-						partial = 1;
-						break;
-					case 'c':
-						ignore_case = 1;
-						break;
-					default:
-						printf("find: Illegal option %c\n", current_char);
-						argc = 0;
-						return 0;
-						break;
-				}
-		}
-	} else {
-		printf("Invalid # of command line arguments.\nRun program with ./find -<flags> <pattern>\n");
-		return 0;
+	while (--argc > 0 && (*++argv)[0] == '-') {
+		while (current_char = *++argv[0]) {
+			switch (current_char) {
+				case 's':
+					sorted = 1;
+					break;
+				case 'r':
+					reversed = 1;
+					break;
+				case 'x':
+					except = 1;
+					break;
+				case 'm':
+					matched = 1;
+					break;
+				case 'n':
+					numbered = 1;
+					break;
+				case 'f':
+					first_occurrence = 1;
+					break;
+				case 'p':
+					partial = 1;
+					break;
+				case 'c':
+					ignore_case = 1;
+					break;
+				default:
+					printf("find: Illegal option %c\n", current_char);
+					argc = 0;
+					return 0;
+					break;
+			}
+    }
 	}
-
-	if (++argv) {
-		pattern = argv[0];
-	} else {
-		printf("Pattern missing.");
-		return 0;
-	}
-
+	// if (++argv) {
+ //    pattern = argv[0];
+	// } else {
+	// 	printf("Pattern missing.");
+	// 	return 0;
+	// }
 	if (sorted && reversed) {
 		printf("find: Illegal usage. -s and -r can't be used together\n");
 		return 0;
@@ -143,23 +129,20 @@ int main(int argc, char** argv) {
 	    printf("find: Illegal usage. -x and -p can't be used together\n");
 	    return 0;
 	}
-	
+
+  char* pattern = *argv;
 	int nlines = readlines(lineptr, MAX_LINES);//similar, but not the same as the slide #23 of Chapter 5 partII.pptx
 	int no_of_results = 0;
-	
-	//for(int i = 0; i < nlines; i++)i
-	//	printf("\n%s\n", lineptr[i]);
 	
 	for(int i = 0; i < nlines; i++) {
 		if (matched) {
 			if((strstr_fully_matched(lineptr[i], pattern) != NULL) != except)
 				results[no_of_results++] = i;
 		} else {
-			if ((strstr(lineptr[i], pattern) != NULL) != except)
-				results[no_of_results++] = i;
+			  if ((strstr(lineptr[i], pattern) != NULL) != except)
+				  results[no_of_results++] = i;
 		}
 	}
-	
 	print_results(pattern, matched, first_occurrence, numbered, sorted, partial, reversed, ignore_case, no_of_results);
-    return 0;
+  return 0;
 }
